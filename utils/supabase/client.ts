d619798c37e385e2fa9f06ddc924345d4826cafd
alexpkso <alexpkso@gmail.com>
2026-02-22 13:37:28 +0100

    initial setup: next.js + supabase

---
+++ utils/supabase/client.ts
@@ -0,0 +1,14 @@
+import { createBrowserClient } from '@supabase/ssr'
+
+export function createClient() {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY
+
+  if (!supabaseUrl || !supabaseKey) {
+    throw new Error(
+      'Missing Supabase env: NEXT_PUBLIC_SUPABASE_URL and/or NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY'
+    )
+  }
+
+  return createBrowserClient(supabaseUrl, supabaseKey)
+}
